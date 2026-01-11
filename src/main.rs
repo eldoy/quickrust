5f1ed0c5736c8d201d8b832d9a634328e5c597ba
Vidar Eldøy <vidar@eldoy.com>
2026-01-11 03:13:14 +0100

    new: initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use rquickjs::{Context, Runtime, function::Func, Object};
+use std::fs;
+
+fn main() {
+    let rt = Runtime::new().unwrap();
+    let ctx = Context::full(&rt).unwrap();
+
+    ctx.with(|ctx| -> Result<(), rquickjs::Error> {
+        let global = ctx.globals();
+
+        global.set("hello", Func::from(|| "from Rust"))?;
+
+        let console = Object::new(ctx.clone())?;
+        console.set("log", Func::from(|s: String| {
+            println!("{}", s);
+        }))?;
+        global.set("console", console)?;
+
+        let code = fs::read_to_string("script.js")?;
+        ctx.eval::<(), _>(code)?;
+
+        Ok(())
+    }).unwrap();
+}
